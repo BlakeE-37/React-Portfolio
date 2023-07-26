84d11d7dae165af3a2a896651fa97e14dfaaee6c
Blake H <blake.hollingsworth80@gmail.com>
2023-07-26 16:13:44 -0600

    added header.css as well as some styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./header.css"
 
 function Header() {
 
     return (
-        <p>Hello World</p>
+        <div className="headerContainer">
+            <img className="headerImg" alt="My wife and Myself" src="portrait.jpg"></img>
+            <h1 className="nameTitle">Blake Hollingsworth</h1>
+            <ul>
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
     )
 };
 
