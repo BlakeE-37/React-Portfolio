a6659ef2665f2ed91595d50ad700d0486375ded3
Blake H <blake.hollingsworth80@gmail.com>
2023-07-26 17:04:39 -0600

    changing how I will style the page, color added to readme

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,22 @@
 import React from "react";
-import "./header.css"
+import Navbar from "./navbar"
+
+const styles = {
+    header: {
+        width: "100%",
+        height: '20vh',
+        background: '#9a74db',
+        display: 'flex'
+    }
+}
 
 function Header() {
 
     return (
-        <div className="headerContainer">
+        <div style={styles.header}>
             <img className="headerImg" alt="My wife and Myself" src="portrait.jpg"></img>
             <h1 className="nameTitle">Blake Hollingsworth</h1>
-            <ul>
-                <li>Home</li>
-                <li>About Me</li>
-                <li>Contact Me</li>
-            </ul>
+            <Navbar />
         </div>
     )
 };
