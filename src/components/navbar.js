f17e97713922724ce8364a2fd26f7b396c073e1d
Blake H <blake.hollingsworth80@gmail.com>
2023-07-27 16:38:13 -0600

    created navbar highlite for active page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,20 +2,20 @@ import React from "react";
 import './components.css'
 
 function Navbar({ handlePageChange, activePage }) {
-
+    // className={activePage === 'aboutMe' ? "links activeLink" : "links"}
     return (
         <ul className="navbar">
-            <li>
-                <a className="links" href="#aboutMe" onClick={() => handlePageChange('aboutMe')}>About Me</a>
+            <li className={activePage === 'aboutMe' ? "linkContainer activeLinkContainer" : "linkContainer"}>
+                <a className="linkText" href="#aboutMe" onClick={() => handlePageChange('aboutMe')}>About Me</a>
             </li>
-            <li >
-                <a className="links" href="#portfolio" onClick={() => handlePageChange('portfolio')}>Portfolio</a>
+            <li className={activePage === 'portfolio' ? "linkContainer activeLinkContainer" : "linkContainer"}>
+                <a className="linkText" href="#portfolio" onClick={() => handlePageChange('portfolio')}>Portfolio</a>
             </li>
-            <li >
-                <a className="links" href="#contact" onClick={() => handlePageChange('contact')}>Contact</a>
+            <li className={activePage === 'contact' ? "linkContainer activeLinkContainer" : "linkContainer"}>
+                <a className="linkText" href="#contact" onClick={() => handlePageChange('contact')}>Contact</a>
             </li>
-            <li >
-                <a className="links" href="#resume" onClick={() => handlePageChange('resume')}>Resume</a>
+            <li className={activePage === 'resume' ? "linkContainer activeLinkContainer" : "linkContainer"}>
+                <a className="linkText" href="#resume" onClick={() => handlePageChange('resume')}>Resume</a>
             </li>
         </ul>
     )
