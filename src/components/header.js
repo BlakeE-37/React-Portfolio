002d1137b07b9de909fc5d58010e18b686e79c9c
Blake H <blake.hollingsworth80@gmail.com>
2023-07-27 10:56:06 -0600

    moved header css to css file

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,13 @@
 import React from "react";
 import Navbar from "./navbar"
-
-const styles = {
-    header: {
-        width: '100%',
-        height: '20vh',
-        background: '#303030',
-        display: 'flex',
-        color: '#EBF2FA'
-    },
-
-    h1: {
-        margin: 10,
-        marginTop: 0,
-        fontSize: '8vh'
-    },
-
-    portrait: {
-        margin: 10
-    }
-
-}
+import './components.css'
 
 function Header() {
 
     return (
-        <div style={styles.header}>
-            <img style={styles.portrait} alt="My wife and Myself" src="portrait.jpg"></img>
-            <h1 style={styles.h1}>Blake Hollingsworth</h1>
+        <div className="header">
+            <img className="portrait" alt="My wife and Myself" src="portrait.jpg"></img>
+            <h1 className="h1">Blake Hollingsworth</h1>
             <Navbar />
         </div>
     )
