747ef486637f614ff40a583916686e09c32b867e
Blake H <blake.hollingsworth80@gmail.com>
2023-07-27 15:08:28 -0600

    added font awesome with npm and added the first icon

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import './components.css'
+
+const element = <FontAwesomeIcon icon={faInstagram} className="socialLink" />
 
 function Footer() {
     return (
         <footer className="footer">
-            {/* <a></a>
+            <a>{element}</a>
             <a></a>
-            <a></a> */}
+            <a></a>
         </footer>
     )
 }
