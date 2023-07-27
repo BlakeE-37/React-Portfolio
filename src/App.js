0a4018ade5063dfcafae79b887b20951b7d90057
Blake H <blake.hollingsworth80@gmail.com>
2023-07-27 16:04:51 -0600

    added main content function logic to choose what page to render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/header"
 import Footer from './components/footer'
+import MainContent from "./components/mainContent";
 import "./App.css"
 
 const App = () => {
     return (
         <div className="mainContainer">
             <Header />
+            <MainContent />
             <Footer />
         </div>
     )
--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import AboutMe from './pages/aboutMe'
 import Portfolio from './pages/portfolio'
 import Resume from './pages/resume'
-import Contact from './pages/Contact'
+import Contact from './pages/contact'
 
-function mainContent() {
+function MainContent() {
 
+    const [activePage, setActivePage] = useState('aboutMe')
+
+    function renderActivePage() {
+        switch (activePage) {
+            case 'aboutMe':
+                return <AboutMe />
+            case 'portfolio':
+                return <Portfolio />
+            case 'Resume':
+                return < Resume />
+            case 'Contact':
+                return <Contact />
+        }
+    }
+
+    return (
+        <div>
+            {renderActivePage()}
+        </div>
+    )
 }
+
+export default MainContent
