59e2b8547606dcf11683de30d10c739fd931ab9e
Blake H <blake.hollingsworth80@gmail.com>
2023-07-27 15:25:02 -0600

    finished footer social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './components.css'
 
-const element = <FontAwesomeIcon icon={faInstagram} className="socialLink" />
+const githubElement = <FontAwesomeIcon icon={faGithub} className="socialLink" />
+const linkedinElement = <FontAwesomeIcon icon={faLinkedin} className="socialLink" />
+const instagramElement = <FontAwesomeIcon icon={faInstagram} className="socialLink" />
 
 function Footer() {
     return (
         <footer className="footer">
-            <a>{element}</a>
-            <a></a>
-            <a></a>
+            <a href="https://github.com/BlakeE-37" target="_blank">{githubElement}</a>
+            <a href="https://www.linkedin.com/in/blakeevanhollingsworth/" target="_blank">{linkedinElement}</a>
+            <a href="https://www.instagram.com/blake.e717/" target="_blank">{instagramElement}</a>
         </footer>
     )
 }
