186e678121fcc49367601325b8d25f11bf0be145
Blake H <blake.hollingsworth80@gmail.com>
2023-07-27 15:37:04 -0600

    added main content component to parent each page

---
+++ src/components/mainContent.js
@@ -0,0 +1,9 @@
+import React from "react";
+import AboutMe from './pages/aboutMe'
+import Portfolio from './pages/portfolio'
+import Resume from './pages/resume'
+import Contact from './pages/Contact'
+
+function mainContent() {
+
+}
