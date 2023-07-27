1d758d39900e813d31740b6bee083ad935eb32e1
Blake H <blake.hollingsworth80@gmail.com>
2023-07-27 12:16:57 -0600

    deleted font awesome and added some footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,9 @@ import './components.css'
 function Footer() {
     return (
         <footer className="footer">
-
+            {/* <a></a>
+            <a></a>
+            <a></a> */}
         </footer>
     )
 }
