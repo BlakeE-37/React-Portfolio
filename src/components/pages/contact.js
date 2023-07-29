23a5ad1d30f72e6a31321b7f34b248632b47f6a3
Blake H <blake.hollingsworth80@gmail.com>
2023-07-29 17:00:32 -0600

    created and styled contact form - logic still needed

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,8 +1,48 @@
 import React from "react";
+import './pages.css'
 
 function Contact() {
+
+    function handleInputChange() {
+        return
+    }
+
+    function handleFormSubmit() {
+        return
+    }
+
     return (
-        <p>Contact</p>
+        <div className="pageContainer">
+            <div className="formContainer">
+                <h2 className="contactTitle">Contact Me</h2>
+                <form className="form">
+                    <input
+                        className="contactName"
+                        name="name"
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder="Name"
+                    />
+                    <input
+                        className="contactEmail"
+                        name="email"
+                        onChange={handleInputChange}
+                        type="email"
+                        placeholder="Email"
+                    />
+                    <textarea
+                        className="contactMessage"
+                        name="message"
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder="Message"
+                    />
+                    <button type="button" className="contactSubmit" onClick={handleFormSubmit}>
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
 
